3ce5636638771035f3689426d7e03399b493199b
awesomehd1 <earthgood83@gmail.com>
2018-07-14 21:00:05 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,20 +2,31 @@ const discord = require("discord.js")
 const fs = require("fs")
 
 const prefix = "bb!"
-client.on ("message", (message) => {
+client.on('ready', () => {
+    console.log('bot launched')
+    bot.user.setStatus('dnd')
+    bot.user.setGame('10/10 bot lmao')
+});
 
+
+client.on('ready', () => {
     msg = message.author.bot.toLowerCaste();
 
     if (message.author.bot) return;
 
     mention = message.mentions.users.first();
 
-    if (msg.startWith (prefix + "Testdm")) {
-        if (mention = null) { return; }
-        message.delete();
-        mentionMessage = Message.content.slice (8);
-        mention.sendMessage (mentionMessage)
-        message.channel.send ();
+    if (command === "dm") {
+        if (!message.author.id === "337024000330956811" && !message.author.id === "338192747754160138") return;
+        
+        var dmdu = (args[0]).replace(/@/g, "")
+        var dmdo = (dmdu).replace(/!/g, "")
+        var dmdi = (dmdo).replace(/>/g, "")
+        var dmed = (dmdi).replace(/</g, "")
+
+        client.users.get(dmed).send("Hi");
+        message.channel.send("Done!")
+
     }
 });
 
